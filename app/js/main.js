71893d6b24a85e7d9798495a3d8a779d9f361eb5
ThunderInTheGrass <propeller8924@gmail.com>
2023-04-09 20:06:23 +0800

    hero

--- app/js/main.js
+++ app/js/main.js
@@ -16,13 +16,80 @@
 
 function cityPopup() {
   const cityBtn = document.querySelector(".header__city-btn");
+  const cityCorrectBtn = document.querySelector('.city__correct-btn');
+  const citySelectBtn = document.querySelector('.city__select-btn');
+  const cityPopupBody = document.querySelectorAll('.city--popup')
   const cityPopup = document.querySelector(".city__popup");
+  const cityPopupCloseBtn = document.querySelector('.city__popup-close-btn');
+  
 
   cityBtn.addEventListener("click", () => {
     cityPopup.classList.toggle("d-none");
   });
+
+  cityPopupCloseBtn.addEventListener("click", () => {
+    cityPopup.classList.toggle("d-none");
+  });
+
+  cityCorrectBtn.addEventListener('click', () => {
+    cityPopup.classList.toggle("d-none");
+  })
+
+  citySelectBtn.addEventListener('click', () => {
+    cityPopup.classList.toggle("d-none");
+  })
+
+  document.body.addEventListener('click', (e) => {
+    if (!e.target.classList.contains('city--popup') && !cityPopup.classList.contains("d-none")) {
+      cityPopup.classList.add("d-none");
+    }
+  })
 }
 cityPopup();
+
+function cityModal() {
+  const btns = document.querySelectorAll(".popup-path");
+  const modalOverlay = document.querySelector(".modal-overlay ");
+  const modals = document.querySelectorAll(".modal");
+  const modalCloseBtn = document.querySelector(".modal-close-btn");
+  const close = document.querySelectorAll(".close");
+
+  btns.forEach((el) => {
+    el.addEventListener("click", (e) => {
+      document.body.classList.add('ov-hidden');
+      let path = e.currentTarget.getAttribute("data-path");
+
+      modals.forEach((el) => {
+        el.classList.remove("modal--visible");
+      });
+
+      document
+        .querySelector(`[data-target="${path}"]`)
+        .classList.add("modal--visible");
+        modalOverlay.classList.add("modal-overlay--visible");
+    });
+  });
+
+  modalOverlay.addEventListener("click", (e) => {
+    document.body.classList.remove('ov-hidden');
+      if (e.target == modalOverlay) {
+        modalOverlay.classList.remove("modal-overlay--visible");
+        modals.forEach((el) => {
+          el.classList.remove("modal--visible");
+        });
+      }
+  });
+
+  modalCloseBtn.addEventListener("click", (e) => {
+    document.body.classList.remove('ov-hidden');
+
+    modalOverlay.classList.remove("modal-overlay--visible");
+    modals.forEach((el) => {
+      el.classList.remove("modal--visible");
+    });
+  });
+}
+cityModal();
 
 const swiper = new Swiper(".hero__slider", {
   navigation: {
